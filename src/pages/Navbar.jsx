283f4d21fe8f66d90e7e63563e0c4d1236135a62
Sushey01 <susmagar012@gmail.com>
2025-07-16 13:31:19 +0545

    responsive-doing

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,20 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.css";
 import Logo from "../assets/images/NavLogo.webp";
-import User from "../assets/images/user.avif"
+import User from "../assets/images/user.avif";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className={styles.navbar}>
       <div className={styles.middle}>
-         <div className={styles.logo}>
-            <img src={Logo}></img>
-          </div>
+        <div className={styles.logo}>
+          <img src={Logo}></img>
+        </div>
         <div className={styles.firstNav}>
-         
           <div className={styles.bars}>
             <div className={styles.category}>
-              <select >
+              <select>
                 <option>Select Category</option>
                 <option>Electronics</option>
                 <option>Fashion</option>
@@ -23,15 +24,11 @@ const Navbar = () => {
                 <option>Toys</option>
               </select>
             </div>
-            <div className={ styles.search1 }>
+            <div className={styles.search1}>
               {/* <p>Search for products</p> */}
               <label>
-                <input
-                placeholder='Search for products...'
-                type='text'
-                
-                />
-                </label>
+                <input placeholder="Search for products..." type="text" />
+              </label>
             </div>
           </div>
           <div className={styles.search2}>
@@ -52,15 +49,17 @@ const Navbar = () => {
               <circle cx="11" cy="11" r="8" />
             </svg>
           </div>
-      </div>
+        </div>
       </div>
 
       <div className={styles.secondNav}>
         <div className={styles.user}>
           <img src={User}></img>
         </div>
-        <div className={styles.heart} style={{position:"relative"}}>
-          <div className="bg-red-500 text-white text-xs  font-semibold w-5 h-5 -translate-y-1/2 translate-x-1/2 text-center content-center rounded-full absolute top-0 right-0">1</div>
+        <div className={styles.heart} style={{ position: "relative" }}>
+          <div className="bg-red-500 text-white text-xs  font-semibold w-5 h-5 -translate-y-1/2 translate-x-1/2 text-center content-center rounded-full absolute top-0 right-0">
+            1
+          </div>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="20"
@@ -77,8 +76,10 @@ const Navbar = () => {
             <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
           </svg>
         </div>
-        <div className={styles.cart} style={{position:"relative"}}>
-          <div className="bg-red-500 text-white text-xs  font-semibold w-5 h-5 -translate-y-1/2 translate-x-1/2 text-center content-center rounded-full absolute top-0 right-0">1</div>
+        <div className={styles.cart} style={{ position: "relative" }}>
+          <div className="bg-red-500 text-white text-xs  font-semibold w-5 h-5 -translate-y-1/2 translate-x-1/2 text-center content-center rounded-full absolute top-0 right-0">
+            1
+          </div>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="20"
@@ -98,6 +99,26 @@ const Navbar = () => {
           </svg>
         </div>
       </div>
+      {/* <div className="flex content-center float-right items-center space-x-4 ">
+        <button
+          aria-label="Toggle menu"
+          onClick={() => setMenuOpen(!menuOpen)}
+          className=" p-2 block lg:hidden"
+        >
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            stroke-width="0"
+            viewBox="0 0 24 24"
+            class="text-2xl"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M3 4H21V6H3V4ZM9 11H21V13H9V11ZM3 18H21V20H3V18Z"></path>
+          </svg>
+        </button>
+      </div> */}
     </div>
   );
 };
