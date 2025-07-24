c67dfbac4e3b0b4db02addb29e938094df8f4ec0
Sushey01 <susmagar012@gmail.com>
2025-07-24 15:22:08 +0545

    working on the responsive

--- src/pages/FlashSaleContainer.jsx
+++ src/pages/FlashSaleContainer.jsx
@@ -4,7 +4,7 @@ import FlashSalePage from '../components/FlashSalePage'
 const FlashSaleContainer = () => {
   return (
     <>
-    <div className='flex gap-3 py-2 px-4'>
+    <div className='md:flex gap-3 py-2 px-4'>
       <FlashSalePage/>
       <FlashSalePage/>
     </div>
