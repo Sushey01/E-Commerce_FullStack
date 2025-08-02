2e6984d3679f0bfe90ebacfeb3f38ff73e05e309
Sushey01 <susmagar012@gmail.com>
2025-08-02 23:26:24 +0545

    mobilenav

---
+++ src/mobileNav/MobileNavbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  HomeIcon,
+  HeartIcon,
+  ShoppingCartIcon,
+  UserIcon,
+} from '@heroicons/react/24/outline';
+
+const MobileNavbar = () => {
+  return (
+    <div className="fixed bottom-0 w-full bg-white border-t border-gray-200 z-50">
+      <div className="flex justify-around items-center py-2">
+        {/* Home (Active) */}
+        <div className="flex flex-col items-center text-black">
+          <HomeIcon className="h-6 w-6" />
+          <div className="w-1.5 h-1.5 bg-black rounded-full mt-1" />
+        </div>
+
+        {/* Wishlist */}
+        <div className="flex flex-col items-center text-gray-400">
+          <HeartIcon className="h-6 w-6" />
+        </div>
+
+        {/* Cart */}
+        <div className="flex flex-col items-center text-gray-400">
+          <ShoppingCartIcon className="h-6 w-6" />
+        </div>
+
+        {/* Profile */}
+        <div className="flex flex-col items-center text-gray-400">
+          <UserIcon className="h-6 w-6" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MobileNavbar;
