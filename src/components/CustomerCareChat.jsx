ffbcf25d2bd2c06b75929ba8458ecb5f099714e2
Sushey01 <susmagar012@gmail.com>
2025-08-10 12:45:51 +0545

    working-on-the-chat-bot

---
+++ src/components/CustomerCareChat.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const CustomerCareChat = () => {
+  return (
+    <div>
+      <div>
+
+        <p>Customer Support</p>
+      </div>
+      <div>
+        <p className='text-center'>2:56 PM</p>
+        <div>
+            <p>Answer Bot</p>
+            <div>
+                <p>Hello! I am automated assistant ready to help. By using this chat, you consent to it being recorded and used by us and third parties. We cam help you for contact and product details.</p>
+
+            </div>
+
+
+            <div className='border rounded-full '>
+                <input type='text' placeholder='Type a message'/>
+            </div>
+
+            <div>
+                Built with Sowis 
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CustomerCareChat
