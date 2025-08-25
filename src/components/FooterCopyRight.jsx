d9475bab462ce852fac7976322705cf03c31d142
Sushey01 <susmagar012@gmail.com>
2025-08-25 11:36:10 +0545

    just-one-line-change

--- src/components/FooterCopyRight.jsx
+++ src/components/FooterCopyRight.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const FooterCopyRight = () => {
   return (
     <div className="w-full flex justify-center bg-[#DAE0DF] p-3">
-      <p className="text-[#777] items-center">Copyright © Dynamic Technosoft. All rights reserved.</p>
+      <p className="text-[#777] text-sm  md:text-base items-center">Copyright © Dynamic Technosoft. All rights reserved.</p>
     </div>
   );
 };
