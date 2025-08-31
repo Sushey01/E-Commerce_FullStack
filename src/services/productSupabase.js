e3fbefadf282a8ff79bcef192d7e4135318b45f2
Sushey01 <susmagar012@gmail.com>
2025-08-31 13:56:08 +0545

    supabase-integration

---
+++ src/services/productSupabase.js
@@ -0,0 +1,8 @@
+import { supabase } from "@supabase/supabase-js";
+
+export const fetchProducts = async ()=>{
+    const {data, error} = await supabase.from("products").select("*");
+    if (error) throw error;
+    console.log("products", datasdfsdf)
+    return data;
+}
